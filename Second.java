4c1289c240a39dadeefe4d5e17c28a7241c430fa
ahsbth <asraj48342@gmail.com>
2023-04-27 12:43:03 +0530

    understanding for namming java file

---
+++ Second.java
@@ -0,0 +1,30 @@
+public class Second {
+    public static void main(String args[])
+    {
+        System.out.println("Main class Second");
+    }
+}
+class A{
+     void methode1()
+     {
+        System.out.println("Hello A");
+     }
+}
+class B{
+     void methode2()
+     {
+        System.out.println("Hello B");
+     }
+}
+class Class{
+    void methode3()
+    {
+        System.out.println("Hello C");
+    }
+}
+class D{
+    void methode4()
+    {
+        System.out.println("Hello D");
+    }
+}
