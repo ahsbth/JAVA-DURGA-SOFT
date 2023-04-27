b5f4e41f12e7d61df644e5e7b1913bd25987b3de
ahsbth <asraj48342@gmail.com>
2023-04-27 12:26:32 +0530

    understandin class objects

---
+++ First.java
@@ -0,0 +1,24 @@
+class A{
+   public static void main(String args[])
+   {
+    System.out.println("Hello A Class")
+   }
+}
+class B{
+  public static void main(String args[])
+  {
+    System.out.println("Hello B class");
+  }
+}
+class C{
+    public static void main(String args[])
+    {
+        System.out.println("Hello C Class");
+    }
+}
+class D{
+    public static void main(String args[])
+    {
+        System.out.println("Hello D Class");
+    }
+}
