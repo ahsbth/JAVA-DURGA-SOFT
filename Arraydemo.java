3c50c9e0cc1614a093de819aefffd394068a4585
ahsbth <asraj48342@gmail.com>
2023-05-02 11:34:37 +0530

    Array declaration

---
+++ Arraydemo.java
@@ -0,0 +1,13 @@
+public class Arraydemo {
+public static void main(String[] args)
+{  
+    // Two Dimentional Array can be Declared as in the following way
+    int[][] a;
+    int b[][];
+    int [][]c;
+    int[] d[];
+    int[] []e;
+    int[]f [];
+    int []g[]; 
+}
+}
